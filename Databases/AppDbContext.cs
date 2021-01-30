7cfcf4faf9e4aa607477b165d94460b1632a2438
cuidanlu9128 <cuidanlu9128@gmail.com>
2021-01-30 01:38:03 -0900

    first commit

---
+++ Databases/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using FakeXiecheng.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace FakeXiecheng.API.Databases
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<TouristRoute> touristRoutes { get; set; }
+        public DbSet<TouristRoutePicture> touristRoutePictures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var touristRouteJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Databases/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePictureData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Databases/touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePictureData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
